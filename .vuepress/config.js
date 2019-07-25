bb76480fd26c52224cd9a63c600f6eb9adc83cf6
haitaodesign <15393243613@163.com>
2019-07-25 19:11:26 +0800

    chore: vuepress config

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'learning-Vue.js',
+  description: '深入学习Vue.js以及相关生态',
+  head: [
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico'}]
+  ],
+  serviceWorker: true,
+  port: 8081,
+  base: '/learning-Vue.js/',
+  dest: 'docs'
+}
